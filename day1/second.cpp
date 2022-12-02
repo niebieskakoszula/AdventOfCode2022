b64e6e5afb40282af494469359242becafec3fde
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2022-12-02 16:28:55 +0100

    Create second.cpp

---
+++ day1/second.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string input;
+    unsigned int first = 0, second = 0, third = 0, current = 0;
+    while (getline(cin, input)) {
+        if (input.empty()) {
+            if (first < current) {
+                third = second;
+                second = first;
+                first = current;
+            }
+            else if(second < current) {
+                third = second;
+                second = current;
+            }
+            else if (third < current) {
+                third = current;
+            }
+            current = 0;
+        }
+        else {
+            current += stoi(input);
+        }
+    }
+    cout << first + second + third << endl;
+}
