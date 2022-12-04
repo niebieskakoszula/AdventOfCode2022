0855c3be3674f56bbbf569f3c53daf773da53800
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2022-12-04 07:22:50 +0100

    Create first.cpp

---
+++ day4/first.cpp
@@ -0,0 +1,38 @@
+using System;
+ 
+namespace advent_Day4_part1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input;
+			string[] split;
+			int beginFirst, endFirst, beginSecond, endSecond;
+			int result = 0;
+			do
+			{
+				input = Console.ReadLine();
+				if(string.IsNullOrEmpty(input))
+				{
+					break;
+				}
+ 
+				split = input.Split(',', '-');
+ 
+				beginFirst = Int32.Parse(split[0]);
+				endFirst = Int32.Parse(split[1]);
+				beginSecond = Int32.Parse(split[2]);
+				endSecond = Int32.Parse(split[3]);
+ 
+				if((beginFirst <= beginSecond && endSecond <= endFirst) ||
+					(beginSecond <= beginFirst && endFirst <= endSecond))
+				{
+					result++;
+				}
+			} while (true);
+ 
+			Console.WriteLine(result);
+		}
+	}
+}
